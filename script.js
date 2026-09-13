5e1497dcf9e12b5fe2fddc5ae71769bd413b3f92
unknown <oladejistephen012@gmail.com>
2026-09-13 18:14:49 +0100

    add the reponsive version of the website to the codebase

---
+++ script.js
@@ -0,0 +1,11 @@
+// const mNavbar= document.getElementById('mNavbar')
+
+function showSidebar() {
+    const dropdown = document.querySelector('.dropdown')
+    dropdown.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const dropdown = document.querySelector('.dropdown')
+    dropdown.style.display = 'none'
+}
